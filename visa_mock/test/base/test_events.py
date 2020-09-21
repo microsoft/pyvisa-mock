cfa9bc1e06dfcfab75f9c1b306bbb19feaacb4b5
Ian Smith (COLD LOGIC) <iasmit@microsoft.com>
2020-09-21 15:27:12 -0700

    Added unit test.

---
+++ visa_mock/test/base/test_events.py
@@ -0,0 +1,79 @@
+import pytest
+import time
+from visa_mock.base.register import register_resources
+from visa_mock.base.high_level import MockResource
+from visa_mock.test.mock_instruments import instruments
+from visa_mock.test.mock_instruments.instruments import Mocker5
+
+from visa import ResourceManager
+from pyvisa.errors import VisaIOError
+from pyvisa.constants import StatusCode
+
+
+def test_blocking_read():
+    """
+    Make sure the the measurement takes a while to complete and
+    that other operations are fast.
+    """
+    register_resources(instruments.resources)
+    rc = ResourceManager(visa_library="@mock")
+    res: MockResource = rc.open_resource("MOCK0::mock5::INSTR")
+    meas_time = Mocker5.meas_time
+
+    start_time = time.time()
+    res.write(":instr:channel1:volt 12")
+    end_time = time.time()
+    elapsted = end_time - start_time
+    assert elapsted < meas_time/2
+
+    start_time = time.time()
+    voltage = res.query(":inStrument:channel1:vOlT?")
+    end_time = time.time()
+    elapsted = end_time - start_time
+    assert elapsted > meas_time/2
+    assert voltage == "12.0"
+
+
+def test_non_blocking_read():
+    """
+    Make sure the the non blocking read is fast.
+    """
+    register_resources(instruments.resources)
+    rc = ResourceManager(visa_library="@mock")
+    res: MockResource = rc.open_resource("MOCK0::mock5::INSTR")
+    meas_time = Mocker5.meas_time
+
+    start_time = time.time()
+    res.write(":instr:channel1:volt 12")
+    end_time = time.time()
+    elapsted = end_time - start_time
+    assert elapsted < meas_time/2
+
+    # clear the STB reg
+    voltage = res.write("*CLS")
+
+    # kick off reading
+    start_time = time.time()
+    res.write(":inStrument:channel1:MEAS")
+    end_time = time.time()
+    elapsted = end_time - start_time
+    assert elapsted < meas_time/2
+
+    # Make sure time out happens
+    with pytest.raises(VisaIOError) as e:
+        res.wait_for_srq(0)
+    assert e.value.error_code == StatusCode.error_timeout
+
+    # Wait for event interrupt
+    start_time = time.time()
+    res.wait_for_srq()
+    end_time = time.time()
+    elapsted = end_time - start_time
+    assert elapsted > meas_time/2
+
+    start_time = time.time()
+    voltage = res.query(":inStrument:channel1:REAd?")
+    end_time = time.time()
+    elapsted = end_time - start_time
+    assert elapsted < meas_time/2
+    assert voltage == "12.0"
--- visa_mock/test/mock_instruments/instruments.py
+++ visa_mock/test/mock_instruments/instruments.py
@@ -1,4 +1,8 @@
+from typing import Dict
 from collections import defaultdict
+import time
+from threading import Thread
+
 from visa_mock.base.base_mocker import BaseMocker, scpi
 
 
@@ -105,9 +109,57 @@ class Mocker4(BaseMocker):
         return self._instruments[number]
 
 
+class Mocker5(BaseMocker):
+    """
+
+    Mock a visa inst that uses status byte and event.
+
+    """
+
+    meas_time: float = 1
+
+    def __init__(self, call_delay: float = 0.0) -> None:
+        super().__init__(call_delay=call_delay)
+        self._voltage: Dict[int, float] = defaultdict(lambda: 0.0)
+
+    @scpi(r'\*CLS')
+    def clear_stb(self) -> None:
+        self.stb = 0
+
+    @scpi(r":INSTRument:CHANNEL(.*):VOLTage (.*)")
+    def _set_voltage(self, channel: int, value: float) -> None:
+        self._voltage[channel] = value
+
+    @scpi(r":INSTRument:CHANNEL(.*):VOLTage\?")
+    def _get_voltage(self, channel: int) -> float:
+        return self._run_measurement(channel)
+
+    def _run_measurement(self, channel: int) -> float:
+        time.sleep(self.meas_time)
+        self.stb = 0x40
+        self.set_service_request_event()
+        return self._voltage[channel]
+
+    @scpi(r":INSTRument:CHANNEL(.*):MEASure")
+    def _start_voltage_meas(self, channel: int) -> None:
+        if self.stb & 0x40:
+            # Don't start another measurement until the previous one is cleared.
+            return
+        Thread(
+            target=self._run_measurement,
+            args=(channel,),
+            daemon=True,
+            ).start()
+
+    @scpi(r":INSTRument:CHANNEL(.*):REAd\?")
+    def _read_voltage_meas(self, channel: int) -> float:
+        return self._voltage[channel]
+
+
 resources = {
     "MOCK0::mock1::INSTR": Mocker1(),
     "MOCK0::mock2::INSTR": Mocker2(),
     "MOCK0::mock3::INSTR": Mocker3(),
     "MOCK0::mock4::INSTR": Mocker4(),
+    "MOCK0::mock5::INSTR": Mocker5(),
 }
